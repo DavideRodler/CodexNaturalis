87126bb9337c6daefe400f77293eebc7a5265785
isabellaponzani <isabellaclara.ponzani@mail.polimi.it>
2024-03-22 19:10:23 +0100

    ObjectiveCountingResource - da correggere richiamo dei metodi su istanze sbagliate

--- ProgettoSW/src/main/java/model/objectives/ObjectiveCountingResource.java
+++ ProgettoSW/src/main/java/model/objectives/ObjectiveCountingResource.java
@@ -5,17 +5,25 @@ import model.enums.Suit;
 import java.util.ArrayList;
 
 public class ObjectiveCountingResource extends Objective{
-    public Suit Simbol;
+    public Suit symbol;
 
     public int checkObjective(HashMap<ArrayList<Integer>, Card> table, Card card ) {
+        int points = 0;
+        return switch(symbol){
+            case FUNGI: points = table.getCountFungi() / 3;
+            case PLANT: points = table.getCountPlant() / 3;
+            case ANIMAL: points = table.getCountAnimal() / 3;
+            default: points = table.getCountInsect() / 3;
+        };
+        return points;
 
     }
 
-    public Suit getSimbol() {
-        return Simbol;
+    public Suit getSymbol() {
+        return symbol;
     }
 
     public ObjectiveCountingResource(Suit suit){
-        this.Simbol = suit;
+        this.symbol = suit;
     }
 }
