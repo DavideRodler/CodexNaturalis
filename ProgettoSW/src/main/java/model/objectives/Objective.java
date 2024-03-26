996b787da2826325fdbe17a1fb81b8faac3d8961
isabellaponzani <isabellaclara.ponzani@mail.polimi.it>
2024-03-26 15:41:52 +0100

    ObjectiveCountingResource e ObjectiveCountingGold - da capire come lanciare exception

--- ProgettoSW/src/main/java/model/objectives/Objective.java
+++ ProgettoSW/src/main/java/model/objectives/Objective.java
@@ -1,13 +1,14 @@
 package model.objectives;
 
+import model.PlayingStation;
 import model.cards.Card;
 
 import java.util.ArrayList;
 
 public class Objective {
-//    public int checkObjective(HashMap<ArrayList<Integer>, CardPlaying> table ) {
-//        return card.getPoints(); //base case: points assigned when card is placed
-//    }
+    public int checkObjective(PlayingStation station) {
+        return 0; //base case: points assigned when card is placed
+    }
 
 
 }
