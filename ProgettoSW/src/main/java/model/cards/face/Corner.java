1caa47ffe0226c2081dc74b737cba80f74665ec4
Davide Rodler <158727353+ZioPera1@users.noreply.github.com>
2024-03-26 16:48:21 +0100

    Revert "aggiustamento"
    This reverts commit c9d7945bb616544b6a47ca57579db636d8ba99c5.

--- ProgettoSW/src/main/java/model/cards/face/Corner.java
+++ ProgettoSW/src/main/java/model/cards/face/Corner.java
@@ -4,14 +4,18 @@ public class Corner {
     private boolean covered;
     private Suit drawing;
 
-    public Corner(Suit drawing) {
+    public Corner(boolean covered, Suit drawing) {
 
-        this.covered = false;
+        this.covered = covered;
         this.drawing = drawing;
     }
 
     public boolean isCovered() {
         return covered;
+    }
+
+    public Corner() {
+        this.covered = false;
     }
 
     public Suit getDrawing(){
