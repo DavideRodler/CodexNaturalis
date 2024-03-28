5806a4428303a232bb86fe326b1caf630166d9b2
tommypic <58572007+tommypic@users.noreply.github.com>
2024-03-28 23:24:29 +0100

    aggiunta test positioning

--- ProgettoSW/src/test/java/model/objectives/ObjectivePositioningTest.java
+++ ProgettoSW/src/test/java/model/objectives/ObjectivePositioningTest.java
@@ -1,7 +1,30 @@
 package model.objectives;
 
+import model.PlayingStation;
+import model.testsTameplate.PlayngStationTameplate;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectivePositioningTest {
 
+    @Test
+    public void test_Positioning() {
+        // Creating the PlayingStation
+        PlayingStation station = PlayngStationTameplate.test_3Cards_1Positioning();
+        int punti = station.getFirstSecretObjective().getObjective().checkObjective(station);
+
+        // Checking the result
+        assertEquals(1, punti, "Test failed. You scored " + punti + " points.");
+    }
+
+    @Test
+    public void test_Positioning2() {
+        // Creating the PlayingStation
+        PlayingStation station = PlayngStationTameplate.test_6Cards_2Positioning();
+        int punti = station.getFirstSecretObjective().getObjective().checkObjective(station);
+
+        // Checking the result
+        assertEquals(2, punti, "Test failed. You scored " + punti + " points.");
+    }
 }
