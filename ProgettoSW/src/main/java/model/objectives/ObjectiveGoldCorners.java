8cbb997a0d0771cd358be32a37024d871d31da36
isabellaponzani <isabellaclara.ponzani@mail.polimi.it>
2024-04-03 12:38:34 +0200

    ObjectiveGoldCorners finita - da testare

--- ProgettoSW/src/main/java/model/objectives/ObjectiveGoldCorners.java
+++ ProgettoSW/src/main/java/model/objectives/ObjectiveGoldCorners.java
@@ -6,9 +6,45 @@ import model.cards.Card;
 import java.util.ArrayList;
 
 public class ObjectiveGoldCorners implements Objective {
-    @Override
-    public int checkObjective(PlayingStation station) {
 
-        return 0;
+    public int checkObjective(PlayingStation station, int x, int y) {
+        int points = 0;
+        //alto a dx
+        int i = x+1;
+        int j = y+1;
+        ArrayList<Integer> key1 = new ArrayList<>();
+        key1.add(0, i);
+        key1.add(1, j);
+        if (station.getTable().containsKey(key1)){
+           points = points + 1;
+        }
+        //basso a dx
+        int w = x+1;
+        int z = y-1;
+        ArrayList<Integer> key2 = new ArrayList<>();
+        key2.add(0, w);
+        key2.add(1, z);
+        if (station.getTable().containsKey(key2)){
+            points = points + 1;
+        }
+        //alto a sx
+        int a = x-1;
+        int b = y+1;
+        ArrayList<Integer> key3 = new ArrayList<>();
+        key3.add(0, a);
+        key3.add(1, b);
+        if (station.getTable().containsKey(key3)){
+            points = points + 1;
+        }
+        //basso a sx
+        int h = x-1;
+        int k = y-1;
+        ArrayList<Integer> key4 = new ArrayList<>();
+        key4.add(0, h);
+        key4.add(1, k);
+        if (station.getTable().containsKey(key4)){
+            points = points + 1;
+        }
+        return points;
     }
 }
