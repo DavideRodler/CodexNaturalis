cba2eb0516d7f45e2019cb451c58e567acd1fed2
isabellaponzani <isabellaclara.ponzani@mail.polimi.it>
2024-05-19 11:06:53 +0200

    coorezione codice testPlacements - DA CORREGGERE LOGICA

--- ProgettoSW/src/test/java/model/placingCondition/PlacingConditionTest.java
+++ ProgettoSW/src/test/java/model/placingCondition/PlacingConditionTest.java
@@ -1,5 +1,18 @@
 package model.placingCondition;
+import controller.GameController;
+import exception.ChangedStateException;
+import exception.NotValidMoveException;
+import model.PlayingStation;
+import model.cards.CardObjective;
+import model.enums.DirectionEnum;
+import model.enums.PositionEnum;
+import model.enums.SuitEnum;
+import model.enums.TokenEnum;
+import model.objectives.ObjectiveDiagonal;
+import model.testsTemplate.PlayingStationTemplate;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 public class PlacingConditionTest {
 
 //
@@ -7,31 +20,39 @@ public class PlacingConditionTest {
 //    // coordinates are valid, above a zero corner of a neighboring card
 //    //  Finally I am also testing that the costs and scores are updated correctly
 //    //  with the placement of new cards
-//    @Test
-//    public void test_null_corner() {
-//        // Creating the PlayingStation
-//        PlayingStation station = PlayngStationTameplate.test_null_corner_c();
-//
-//        int punti = station.getPlayer().getPoints();
-//        int animals = station.getCountAnimal();
-//        int plants = station.getCountPlant();
-//        int fungi = station.getCountFungi();
-//        int insects = station.getCountInsect();
-//        int inkwell = station.getCountInkwell();
-//        int quill = station.getCountQuill();
-//        int manuscript = station.getCountManuscript();
-//
-//
+    @Test
+    public void test_null_corner() throws ChangedStateException, NotValidMoveException {
+        // Creating the PlayingStation
+
+        PlayingStation station = PlayingStationTemplate.test_null_corner_c();
+        GameController game = new GameController();
+        game.setPlayerNumber(2);
+        game.addPlayer("isa", TokenEnum.BLUE);
+        game.addPlayer("tommy", TokenEnum.YELLOW);
+        game.getBoard().getPlayer("isa").setStation(station);
+        ObjectiveDiagonal objectivetmp = new ObjectiveDiagonal(DirectionEnum.LEFT, SuitEnum.ANIMAL);
+        CardObjective cardObjectiveTmp = new CardObjective(4, 3, objectivetmp);
+        game.getBoard().getPlayer("isa").setSecretObjective(cardObjectiveTmp);
+        int punti = game.getBoard().getPlayer("isa").getPoints();
+        int animals = station.getCountAnimal();
+        int plants = station.getCountPlant();
+        int fungi = station.getCountFungi();
+        int insects = station.getCountInsect();
+        int inkwell = station.getCountInkwell();
+        int quill = station.getCountQuill();
+        int manuscript = station.getCountManuscript();
+
+
 //        // Checking the result
-//        assertEquals(1, punti, "Test failed. You scored " + punti + " points.");
-//        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
-//        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
-//        assertEquals(2, fungi, "Test failed. You scored " + fungi + " fungi.");
-//        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
-//        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
-//        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
-//        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
-//    }
+        assertEquals(1, punti, "Test failed. You scored " + punti + " points.");
+        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
+        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
+        assertEquals(2, fungi, "Test failed. You scored " + fungi + " fungi.");
+        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
+        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
+        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
+        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
+    }
 //
 //
 //
@@ -41,30 +62,38 @@ public class PlacingConditionTest {
 //    //  Finally I am also testing that the costs and scores are updated correctly
 //    //  with the placement of new cards
 //
-//    @Test
-//    public void test_alone_card() {
-//        // Creating the PlayingStation
-//        PlayingStation station = PlayngStationTameplate.test_alone_card_c();
-//        int punti = station.getPlayer().getPoints();
-//        int animals = station.getCountAnimal();
-//        int plants = station.getCountPlant();
-//        int fungi = station.getCountFungi();
-//        int insects = station.getCountInsect();
-//        int inkwell = station.getCountInkwell();
-//        int quill = station.getCountQuill();
-//        int manuscript = station.getCountManuscript();
+    @Test
+    public void test_alone_card() throws ChangedStateException, NotValidMoveException {
+        // Creating the PlayingStation
+        PlayingStation station = PlayingStationTemplate.test_alone_card_c();
+        GameController game = new GameController();
+        game.setPlayerNumber(2);
+        game.addPlayer("isa", TokenEnum.BLUE);
+        game.addPlayer("tommy", TokenEnum.YELLOW);
+        game.getBoard().getPlayer("isa").setStation(station);
+        ObjectiveDiagonal objectivetmp = new ObjectiveDiagonal(DirectionEnum.LEFT, SuitEnum.ANIMAL);
+        CardObjective cardObjectiveTmp = new CardObjective(4, 3, objectivetmp);
+        game.getBoard().getPlayer("isa").setSecretObjective(cardObjectiveTmp);
+        int punti = game.getBoard().getPlayer("isa").getPoints();
+        int animals = station.getCountAnimal();
+        int plants = station.getCountPlant();
+        int fungi = station.getCountFungi();
+        int insects = station.getCountInsect();
+        int inkwell = station.getCountInkwell();
+        int quill = station.getCountQuill();
+        int manuscript = station.getCountManuscript();
 //
 //
 //        // Checking the result
-//        assertEquals(0, punti, "Test failed. You scored " + punti + " points.");
-//        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
-//        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
-//        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
-//        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
-//        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
-//        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
-//        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
-//    }
+        assertEquals(0, punti, "Test failed. You scored " + punti + " points.");
+        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
+        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
+        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
+        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
+        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
+        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
+        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
+    }
 //
 //
 //
@@ -74,30 +103,38 @@ public class PlacingConditionTest {
 //    // a card in the same position of another card
 //    //  Finally I am also testing that the costs and scores are updated correctly
 //    //  with the placement of new cards
-//    @Test
-//    public void test_out_of_bound() {
-//        // Creating the PlayingStation
-//        PlayingStation station = PlayngStationTameplate.test_out_of_bound_c();
-//        int punti = station.getPlayer().getPoints();
-//        int animals = station.getCountAnimal();
-//        int plants = station.getCountPlant();
-//        int fungi = station.getCountFungi();
-//        int insects = station.getCountInsect();
-//        int inkwell = station.getCountInkwell();
-//        int quill = station.getCountQuill();
-//        int manuscript = station.getCountManuscript();
+    @Test
+    public void test_out_of_bound() throws ChangedStateException, NotValidMoveException {
+        // Creating the PlayingStation
+        PlayingStation station = PlayingStationTemplate.test_out_of_bound_c();
+        GameController game = new GameController();
+        game.setPlayerNumber(2);
+        game.addPlayer("isa", TokenEnum.BLUE);
+        game.addPlayer("tommy", TokenEnum.YELLOW);
+        game.getBoard().getPlayer("isa").setStation(station);
+        ObjectiveDiagonal objectivetmp = new ObjectiveDiagonal(DirectionEnum.LEFT, SuitEnum.ANIMAL);
+        CardObjective cardObjectiveTmp = new CardObjective(4, 3, objectivetmp);
+        game.getBoard().getPlayer("isa").setSecretObjective(cardObjectiveTmp);
+        int punti = game.getBoard().getPlayer("isa").getPoints();
+        int animals = station.getCountAnimal();
+        int plants = station.getCountPlant();
+        int fungi = station.getCountFungi();
+        int insects = station.getCountInsect();
+        int inkwell = station.getCountInkwell();
+        int quill = station.getCountQuill();
+        int manuscript = station.getCountManuscript();
 //
 //
 //        // Checking the result
-//        assertEquals(0, punti, "Test failed. You scored " + punti + " points.");
-//        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
-//        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
-//        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
-//        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
-//        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
-//        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
-//        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
-//    }
+        assertEquals(0, punti, "Test failed. You scored " + punti + " points.");
+        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
+        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
+        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
+        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
+        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
+        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
+        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
+    }
 //
 //
 //
@@ -110,30 +147,38 @@ public class PlacingConditionTest {
 //    //  with the placement of new cards, for example in this case I have also tested
 //    //  that the gold card scoring method which assigns points for each gold resource
 //    //  of the type specified on the card itself , would add the correct score to the player.
-//    @Test
-//    public void test_goldCard_cost() {
-//        // Creating the PlayingStation
-//        PlayingStation station = PlayngStationTameplate.test_goldCard_cost_c();
-//        int punti = station.getPlayer().getPoints();
-//        int animals = station.getCountAnimal();
-//        int plants = station.getCountPlant();
-//        int fungi = station.getCountFungi();
-//        int insects = station.getCountInsect();
-//        int inkwell = station.getCountInkwell();
-//        int quill = station.getCountQuill();
-//        int manuscript = station.getCountManuscript();
+    @Test
+    public void test_goldCard_cost() throws ChangedStateException, NotValidMoveException {
+        // Creating the PlayingStation
+        PlayingStation station = PlayingStationTemplate.test_goldCard_cost_c();
+        GameController game = new GameController();
+        game.setPlayerNumber(2);
+        game.addPlayer("isa", TokenEnum.BLUE);
+        game.addPlayer("tommy", TokenEnum.YELLOW);
+        game.getBoard().getPlayer("isa").setStation(station);
+        ObjectiveDiagonal objectivetmp = new ObjectiveDiagonal(DirectionEnum.LEFT, SuitEnum.ANIMAL);
+        CardObjective cardObjectiveTmp = new CardObjective(4, 3, objectivetmp);
+        game.getBoard().getPlayer("isa").setSecretObjective(cardObjectiveTmp);
+        int punti = game.getBoard().getPlayer("isa").getPoints();
+        int animals = station.getCountAnimal();
+        int plants = station.getCountPlant();
+        int fungi = station.getCountFungi();
+        int insects = station.getCountInsect();
+        int inkwell = station.getCountInkwell();
+        int quill = station.getCountQuill();
+        int manuscript = station.getCountManuscript();
 //
 //
 //        // Checking the result
-//        assertEquals(1, punti, "Test failed. You scored " + punti + " points.");
-//        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
-//        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
-//        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
-//        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
-//        assertEquals(1, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
-//        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
-//        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
-//    }
+        assertEquals(1, punti, "Test failed. You scored " + punti + " points.");
+        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
+        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
+        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
+        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
+        assertEquals(1, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
+        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
+        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
+    }
 //
 //
 //
@@ -141,30 +186,38 @@ public class PlacingConditionTest {
 //    // of another card, or that I can't place a card along the same axes of adjacent cards
 //    //  Finally I am also testing that the costs and scores are updated correctly
 //    //  with the placement of new cards
-//    @Test
-//    public void test_same_axes() {
-//        // Creating the PlayingStation
-//        PlayingStation station = PlayngStationTameplate.test_same_axes_c();
-//        int punti = station.getPlayer().getPoints();
-//        int animals = station.getCountAnimal();
-//        int plants = station.getCountPlant();
-//        int fungi = station.getCountFungi();
-//        int insects = station.getCountInsect();
-//        int inkwell = station.getCountInkwell();
-//        int quill = station.getCountQuill();
-//        int manuscript = station.getCountManuscript();
+    @Test
+    public void test_same_axes() throws ChangedStateException, NotValidMoveException {
+        // Creating the PlayingStation
+        PlayingStation station = PlayingStationTemplate.test_same_axes_c();
+        GameController game = new GameController();
+        game.setPlayerNumber(2);
+        game.addPlayer("isa", TokenEnum.BLUE);
+        game.addPlayer("tommy", TokenEnum.YELLOW);
+        game.getBoard().getPlayer("isa").setStation(station);
+        ObjectiveDiagonal objectivetmp = new ObjectiveDiagonal(DirectionEnum.LEFT, SuitEnum.ANIMAL);
+        CardObjective cardObjectiveTmp = new CardObjective(4, 3, objectivetmp);
+        game.getBoard().getPlayer("isa").setSecretObjective(cardObjectiveTmp);
+        int punti = game.getBoard().getPlayer("isa").getPoints();
+        int animals = station.getCountAnimal();
+        int plants = station.getCountPlant();
+        int fungi = station.getCountFungi();
+        int insects = station.getCountInsect();
+        int inkwell = station.getCountInkwell();
+        int quill = station.getCountQuill();
+        int manuscript = station.getCountManuscript();
 //
 //
 //        // Checking the result
-//        assertEquals(0, punti, "Test failed. You scored " + punti + " points.");
-//        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
-//        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
-//        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
-//        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
-//        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
-//        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
-//        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
-//    }
+        assertEquals(0, punti, "Test failed. You scored " + punti + " points.");
+        assertEquals(3, animals, "Test failed. You scored " + animals + " animals.");
+        assertEquals(1, plants, "Test failed. You scored " + plants + " plants.");
+        assertEquals(1, fungi, "Test failed. You scored " + fungi + " fungi.");
+        assertEquals(1, insects, "Test failed. You scored " + insects + " insects.");
+        assertEquals(0, inkwell, "Test failed. You scored " + inkwell + " inkwell.");
+        assertEquals(0, quill, "Test failed. You scored " + quill + " quill.");
+        assertEquals(0, manuscript, "Test failed. You scored " + manuscript + " manuscript.");
+    }
 
 
 }
