2312af527006dc2cfe0c87247f1b0abc58911466
ericpiotti <eric.piotti@mail.polimi.it>
2024-05-26 23:05:32 +0200

    bozza struttura cli

---
+++ ProgettoSW/src/main/java/View/GUI/StartSceneController.java
@@ -0,0 +1,16 @@
+package View.GUI;
+
+public class StartSceneController {
+
+    //TODO: idealmente la gui dovrebbe essere strutturata cosi:
+    // - creazione del server e del client fatta da terminale
+    // - sempre da terminale si sceglie se usare cli oppure gui
+    // - se viene scelta al gui, si apre questa scena, dove ci sara schermata iniziale e si sceglierà il nome e pedina
+    // - poi scena di attesa che tutti i client si siano collegati
+    // - scena per selezione delle carta iniziale, obiettivo etc...
+    // - scena di gioco effettivo --> station anche degli altri giocatori
+    // - scena della scoreboard
+    // - scena del vincitore
+
+
+}
