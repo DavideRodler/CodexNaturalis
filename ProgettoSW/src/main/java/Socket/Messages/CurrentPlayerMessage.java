7b5127e31ffe862a081edd3bca05e49e2ee73ded
tommypic <tommasopiccoli7575@gmail.com>
2024-06-23 15:49:03 +0200

    Aggiunto message per il currentplayer

---
+++ ProgettoSW/src/main/java/Socket/Messages/CurrentPlayerMessage.java
@@ -0,0 +1,14 @@
+package Socket.Messages;
+
+public class CurrentPlayerMessage extends Message {
+    private String currentPlayer;
+
+    public CurrentPlayerMessage(String currentPlayer) {
+        super("CurrentPlayer");
+        this.currentPlayer = currentPlayer;
+    }
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+}
