7dcc33f08e7c1b9c1ec058efe3744c2ed1aeb42c
Davide Rodler <158727353+ZioPera1@users.noreply.github.com>
2024-06-27 15:47:13 +0200

    metodo scoreboard

--- ProgettoSW/src/main/java/View/GUI/ScoreBoardController.java
+++ ProgettoSW/src/main/java/View/GUI/ScoreBoardController.java
@@ -227,6 +227,22 @@ public class ScoreBoardController implements Initializable{
 
     public void printScores(LinkedHashMap<String, ArrayList<Integer>> map){
 
+        int i = 0;
+        for(Map.Entry<String, ArrayList<Integer>> entry : map.entrySet()){
+            String playerName = entry.getKey();
+            ArrayList<Integer> playerData = entry.getValue();
+            int totalScore = playerData.get(0);
+            int completedObjectives = playerData.get(1);
+
+            String playerInfo = playerName + ": Total Score = " + totalScore + ", Completed Objectives = " + completedObjectives;
+
+            Label label = findLabel(i);
+            if(label != null){
+                label.setText(playerInfo);
+            }
+            i++;
+        }
+
     }
 
 
